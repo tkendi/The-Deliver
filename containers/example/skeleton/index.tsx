67b9957be5a28ff5f57707e13f5e03817aa4be16
ChaMyungjun <acg70159@gmail.com>
2021-08-21 18:01:46 +0900

    examples fix skeleton

--- containers/example/skeleton/index.tsx
+++ containers/example/skeleton/index.tsx
@@ -3,15 +3,23 @@ import styled from "styled-components";
 
 const Skeleton = () => {
   return (
-    <SkeletonItem>
-      <div>
-        <SkeletonImg />
-      </div>
-      <SkeletonInfo>
-        <SkeletonName />
-        <SkeletonEmail />
-      </SkeletonInfo>
-    </SkeletonItem>
+    <>
+      <SkeletonItem>
+        <div>
+          <SkeletonImg />
+        </div>
+        <SkeletonInfo>
+          <SkeletonName />
+          <SkeletonEmail />
+        </SkeletonInfo>
+      </SkeletonItem>
+
+      <SkeletonItem>
+        <div>
+          <SkeletonVertical />
+        </div>
+      </SkeletonItem>
+    </>
   );
 };
 
@@ -34,15 +42,21 @@ const SkeletonImg = styled.div`
   background: #f2f2f2;
   position: relative;
   overflow: hidden;
-  :before {
+  ::before {
     content: "";
     position: absolute;
     top: 0;
     left: 0;
     width: 30px;
     height: 100%;
-    background: linear-gradient(to right, #f2f2f2, #ddd, #f2f2f2);
-    animation: loading 4s infinite linear;
+    background: linear-gradient(
+      90deg,
+      rgba(190, 190, 190, 0.2) 25%,
+      rgba(129, 129, 129, 0.24) 37%,
+      rgba(190, 190, 190, 0.2) 63%
+    );
+    background-size: 400% 100%;
+    animation: loading 3s infinite linear;
 
     @keyframes loading {
       0% {
@@ -50,7 +64,7 @@ const SkeletonImg = styled.div`
       }
       50%,
       100% {
-        transform: translateX(460px);
+        transform: translateX(300px);
       }
     }
   }
@@ -71,14 +85,20 @@ const SkeletonName = styled.p`
   position: relative;
   overflow: hidden;
 
-  :before {
+  ::before {
     content: "";
     position: absolute;
     top: 0;
     left: 0;
     width: 30px;
     height: 100%;
-    background: linear-gradient(to right, #f2f2f2, #ddd, #f2f2f2);
+    background: linear-gradient(
+      90deg,
+      rgba(190, 190, 190, 0.2) 25%,
+      rgba(129, 129, 129, 0.24) 37%,
+      rgba(190, 190, 190, 0.2) 63%
+    );
+    background-size: 400% 100%;
     animation: loading 3s infinite linear;
 
     @keyframes loading {
@@ -101,14 +121,55 @@ const SkeletonEmail = styled.p`
   position: relative;
   overflow: hidden;
 
-  :before {
+  ::before {
     content: "";
     position: absolute;
     top: 0;
     left: 0;
     width: 30px;
     height: 100%;
-    background: linear-gradient(to right, #f2f2f2, #ddd, #f2f2f2);
+    background: linear-gradient(
+      90deg,
+      rgba(190, 190, 190, 0.2) 25%,
+      rgba(129, 129, 129, 0.24) 37%,
+      rgba(190, 190, 190, 0.2) 63%
+    );
+    background-size: 400% 100%;
+    animation: loading 3s infinite linear;
+
+    @keyframes loading {
+      0% {
+        transform: translateX(0);
+      }
+      50%,
+      100% {
+        transform: translateX(300px);
+      }
+    }
+  }
+`;
+
+const SkeletonVertical = styled.div`
+  width: 30px;
+  height: 300px;
+  background: #f2f2f2;
+  position: relative;
+  overflow: hidden;
+
+  ::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 30px;
+    height: 100%;
+    background: linear-gradient(
+      90deg,
+      rgba(190, 190, 190, 0.2) 25%,
+      rgba(129, 129, 129, 0.24) 37%,
+      rgba(190, 190, 190, 0.2) 63%
+    );
+    background-size: 400% 100%;
     animation: loading 3s infinite linear;
 
     @keyframes loading {

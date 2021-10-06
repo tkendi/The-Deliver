40b2e96fde68d1e7a3876f416568533c6615e664
ChaMyungjun <acg70159@gmail.com>
2021-10-06 15:08:49 +0900

    tset react-slick centerPadding

--- pages/example/slick/index.tsx
+++ pages/example/slick/index.tsx
@@ -1,45 +1,97 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-    return (
-      <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <Item>
-            <h3>1</h3>
-          </Item>
-          <Item>
-            <h3>2</h3>
-          </Item>
-          <Item>
-            <h3>3</h3>
-          </Item>
-          <Item>
-            <h3>4</h3>
-          </Item>
-          <Item>
-            <h3>5</h3>
-          </Item>
-          <Item>
-            <h3>6</h3>
-          </Item>
-        </Slider>
-      </div>
-    );
+const Slide = () => {
+  // 슬라이드 설정
+  const settings = {
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000, // 넘어가는 속도
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "300px",
+  };
+
+  const sliders = [
+    { name: "1", image: "https://source.unsplash.com/random/690x386" },
+    { name: "2", image: "https://source.unsplash.com/random/690x386" },
+    { name: "3", image: "https://source.unsplash.com/random/690x386" },
+    { name: "4", image: "https://source.unsplash.com/random/690x386" },
+    { name: "5", image: "https://source.unsplash.com/random/690x386" },
+    { name: "6", image: "https://source.unsplash.com/random/690x386" },
+  ];
+
+  return (
+    <Container>
+      <SlideTitle>인기 서비스</SlideTitle>
+      <StyledSlider {...settings}>
+        {sliders.map(({ name, image }: any) => {
+          return (
+            <CardBox>
+              <CardImg alt="인기 서비스" src={image} />
+              <CardText>{name}</CardText>
+            </CardBox>
+          );
+        })}
+      </StyledSlider>
+    </Container>
+  );
+};
+
+export default Slide;
+
+const SlideTitle = styled.h2`
+  padding: 60px 0px 50px 0px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bolder;
+`;
+
+const StyledSlider = styled(Slider)`
+  .slick-list {
+    max-width: 690px;
+    /* width: 1600px; */
+    margin: auto;
   }
-}
 
-const Item = styled.div`
-  width: calc(100% / 6);
-  background-color: rgba(122, 50, 255, 0.1);
+  .slick-slide div {
+    /* cursor: pointer; */
+  }
+
+  .slick-dots {
+    bottom: -50px;
+    margin-top: 200px;
+  }
+
+  .slick-track {
+    /* overflow-x: hidden; */
+  }
+`;
+
+const Container = styled.div`
+  margin-right: 25px;
+`;
+
+const CardBox = styled.div`
+  max-width: 600px;
+  height: max-content;
+  cursor: pointer;
+`;
+
+const CardImg = styled.img`
+  max-width: 600px;
+  height: 386px;
+  margin: auto;
+`;
+
+const CardText = styled.p`
+  /* padding: 20px; */
+  font-size: 20px;
+  font-weight: bolder;
+  text-align: center;
 `;

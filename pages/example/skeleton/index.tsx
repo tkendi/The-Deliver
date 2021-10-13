8024642a3abd08b44364c7398743f567cc237c41
ChaMyungjun <acg70159@gmail.com>
2021-10-13 12:13:06 +0900

    test severside props cache control setting

--- pages/example/skeleton/index.tsx
+++ pages/example/skeleton/index.tsx
@@ -3,3 +3,18 @@ import SkeletonContainer from "containers/example/skeleton";
 const ExampleSkeletonPage = () => <SkeletonContainer />;
 
 export default ExampleSkeletonPage;
+
+// export async function getServerSideProps({ req, res }: any) {
+//   //   console.log("REQ", req);
+//   //   console.log("RES", res);
+//   res.setHeader(
+//     "Cache-Control",
+//     "public, s-maxage=100, stale-while-revalidate=59"
+//   );
+
+//   return {
+//     props: {
+//       time: new Date().toISOString(),
+//     },
+//   };
+// }
